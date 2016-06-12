2e4826ac34f1f8bba2b74ef1c89f5b70fbd729ea
Raul Sanchez Liebana <raul.sanchez.liebana@bbva.com>
2016-06-12 11:18:36 +0200

    Updated from image version to 0.5.18

---
+++ root/opt/nginx/bin/nginx-config.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+cat << EOF > ${SERVICE_HOME}/conf/nginx.conf
+user  ${SERVICE_USER};
+worker_processes  2;
+
+error_log  ${SERVICE_HOME}/log/error.log warn;
+pid        ${SERVICE_HOME}/run/nginx.pid;
+
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    include       ${SERVICE_HOME}/conf/mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log  ${SERVICE_HOME}/log/access.log  main;
+
+    sendfile        off;
+    #tcp_nopush     on;
+
+    keepalive_timeout  65;
+
+    #gzip  on;
+
+    include ${SERVICE_HOME}/sites/*.conf;
+}
+EOF
+
+cat << EOF > ${SERVICE_HOME}/sites/example.conf
+server {
+        listen 8080 default_server;
+
+        root ${SERVICE_HOME}/www;
+        index index.html index.htm;
+
+        # Make site accessible from http://localhost/
+        server_name localhost;
+
+        location / {
+
+                try_files \$uri \$uri/ /index.html;
+
+        }
+}
+EOF
